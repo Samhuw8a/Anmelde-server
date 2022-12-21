9992d862c6dcefa13a3cb8e7ebd98aa3705bd1ec
Samhuw8a <Samuel.huwiler@gmx.ch>
2022-12-21 11:01:22 +0100

    added remake

---
+++ main.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+import pandas as pd
+import time
+from configparser import ConfigParser
+import smtplib
+import ssl
+import sys
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from mcrcon import MCRcon
+import random
+
+def main()->None:
+    pass
+
+if __name__ =="__main__":
+    main()
