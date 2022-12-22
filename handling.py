8af9aca826e5a9308774188f5f9fb2a6c4ffbdf0
Samhuw8a <Samuel.huwiler@gmx.ch>
2022-12-22 12:56:56 +0100

    handling

--- handling.py
+++ handling.py
@@ -1,3 +1,10 @@
+from   configparser import ConfigParser
+from   mcrcon import MCRcon
+import sqlalchemy
+import pandas as pd
+import time
+import random
+
 class User():
     def __init__(self,mail:str,username:str,name:str) -> None:
         self.mail:str     = mail 
@@ -7,7 +14,45 @@ class User():
         return f"User({self.mail},{self.username},{self.name})"
 
 class Parser():
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def load_config(self,path:str)->dict:
+        config=ConfigParser(interpolation=None)
+        config.read(path)
+        return {
+        "db_username"     : str(config['credentials']['user']),
+        "db_password"     : str(config['credentials']['password_db']),
+        "db_server_ip"    : str(config['db']['server_ip']),
+        "db_database"     : str(config['db']['db']),
+        "db_table"        : str(config['db']['table']),
+        "mail_password"   : str(config['credentials']['password_web']),
+        "mcrcon_password" : str(config['credentials']['mcpassword']),
+        }
+
+    def get_user(self, dbframe:pd.DataFrame)->User:
+        mail = dbframe["reg_mail"]
+        username = dbframe["reg_username"]
+        name = dbframe["reg_name"]
+        return User(mail,username,name)
 
 class Handler():
-    pass
+
+    def __init__(self,username:str,pswrd:str,server_ip:str) -> None:
+        self.username:str = username
+        self.password:str = pswrd
+        self.ip:str       = server_ip
+
+    def sql_call(self,cmd:str)->pd.DataFrame:
+        engine = sqlalchemy.create_engine(
+            f"mysql+pymysql://{self.username}:{self.password}@{self.ip}/Registration")
+        return pd.read_sql(str(cmd),
+                           con=engine
+                          )
+
+def main()->None:
+    p = Parser()
+    print(p.load_config("test_config.ini"))
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import sqlalchemy
-import pandas as pd
-import time
-from   configparser import ConfigParser
-from   mcrcon import MCRcon
-import random
-
 from handling import Handler
 from email_handler import Email_server
 from errors import Error_handler
