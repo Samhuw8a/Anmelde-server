2311ead3b6f7f78ce2a2e425c56e98301667e555
Samhuw8a <Samuel.huwiler@gmx.ch>
2023-02-14 12:43:40 +0100

    Msg loading optional

--- src/email_handler.py
+++ src/email_handler.py
@@ -5,6 +5,7 @@ import os
 from handling import User
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from typing import Optional,Any
 
 
 class Email_server:
@@ -31,7 +32,8 @@ class Email_server:
         with open(os.path.dirname(__file__) + file_name, "r") as f:
             self.message = f.read().strip()
 
-    def send(self, user: User, subject: str) -> None:
+    def send(self, user: User, subject: str, msg_inp:Optional[str] = None) -> None:
+        msg_str:Any = self.message if msg_inp else msg_inp
         self.message = self.message.replace("{mail}", user.mail)
         self.message = self.message.replace("{token}", str(user.token))
         self.message = self.message.replace("{name}", user.name)
@@ -40,7 +42,7 @@ class Email_server:
         msg["From"] = self.sender
         msg["To"] = user.mail
         msg["Subject"] = subject
-        msg.attach(MIMEText(self.message, "plain", "utf-8"))
+        msg.attach(MIMEText(msg_str, "plain", "utf-8"))
         text = msg.as_string()
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(self.server, self.port, context=context) as server:
@@ -54,8 +56,8 @@ def main() -> None:
         log, 465, "cap.ssl.hosttech.eu", "**************", "no_reply@ksrminecraft.ch"
     )
     samuel = User(mail="samuel.huwiler@gmx.ch", username="samhuw_8a", name="Samuel")
-    serv.load_from_template("/../tests/test_mail.txt")
-    serv.send(samuel, "Test_mail")
+    #  serv.load_from_template("/../tests/test_mail.txt")
+    serv.send(samuel, "Test_mail","/../tests/test_mail.txt")
 
 
 if __name__ == "__main__":
